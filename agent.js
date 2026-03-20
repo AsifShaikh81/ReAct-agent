098107b0f70ae2f743db41e83808c7ebb95c0554
AsifShaikh81 <shaikhasif78866@gmail.com>
2026-03-21 01:05:11 +0530

    21-3

--- agent.js
+++ agent.js
@@ -1,10 +1,10 @@
-import {readlink, writeFileSync} from "node:fs"
+import { readlink, writeFileSync } from "node:fs";
 import { ChatGroq } from "@langchain/groq";
 import { createAgent, tool } from "langchain";
 import { TavilySearch } from "@langchain/tavily";
 import z from "zod";
-import { Readline } from "node:readline/promises";
-import { stdout } from "node:process";
+import readline from "node:readline/promises";
+import { MemorySaver } from "@langchain/langgraph";
 
 // https://github.com/langchain-ai/langgraphjs (refer this)
 // https://docs.langchain.com/oss/javascript/langchain/agents
@@ -17,24 +17,28 @@ async function main(params) {
     maxResults: 3,
     topic: "general",
   });
- //*custom tool
+  //*custom tool
   const CalenderEvents = tool(
-  async ({ query }) => {
-    // hardcoded for learning
-    return JSON.stringify([{
-      Title:"Meeting",
-      Time:"1pm",
-      Location:"Zoom"
-    }])
-  },
-  {
-    name: "get-calender-events",
-    description: "Call to get calender events.",
-    schema: z.object({
-      query: z.string().describe("The query to use in calender event search."),
-    }),
-  }
-);
+    async ({ query }) => {
+      // hardcoded for learning
+      return JSON.stringify([
+        {
+          Title: "Meeting",
+          Time: "1pm",
+          Location: "Zoom",
+        },
+      ]);
+    },
+    {
+      name: "get-calender-events",
+      description: "Call to get calender events.",
+      schema: z.object({
+        query: z
+          .string()
+          .describe("The query to use in calender event search."),
+      }),
+    },
+  );
 
   //*model created
   const model = new ChatGroq({
@@ -42,37 +46,49 @@ async function main(params) {
     temperature: 0,
   });
 
-  //*agent created
+  //*agent created and memory added
+  const checkpointer = new MemorySaver();
   const agent = createAgent({
     model: model,
     tools: [Searchtool, CalenderEvents],
+    checkpointer: checkpointer, // memory
   });
 
   //*invoke agent
-  const result = await agent.invoke({
-    messages: [
-      {
-        role:"system",
-        content :`You are a personal assistant. Use provided tools to get the information if you don't have it.`
-      },
-      {
-        role: "user",
-        content: "Do i have any meeting scheduled",
-      },
-    ],
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
   });
+  while (true) {
+    const userQuery = await rl.question("You: ");
+    if (userQuery === "/bye") break;
+    const result = await agent.invoke(
+      {
+        messages: [
+          {
+            role: "system",
+            content: `You are name is Jarvis a personal assistant. Use provided tools to get the information if you don't have it.`,
+          },
+          {
+            role: "user",
+            content: userQuery,
+          },
+        ],
+      },
+      { configurable: { thread_id: "1" } }, // Memory added
+    );
 
-  const rl = Readline.createInterface({input:process.stdin, output:stdout})
+    console.log("AI: ", result.messages[result.messages.length - 1].content);
+  }
+  rl.close();
 
-  //To visualize by graph, how agent is working  
- const drawGraph =  await agent.getGraphAsync()
- const graphStateImage = await drawGraph.drawMermaidPng()
- const graphStateArrayBuffer = await graphStateImage.arrayBuffer()
+  //To visualize by graph, how agent is working
+  const drawGraph = await agent.getGraphAsync();
+  const graphStateImage = await drawGraph.drawMermaidPng();
+  const graphStateArrayBuffer = await graphStateImage.arrayBuffer();
 
- const filePath = './graphState.png'
- writeFileSync(filePath, new Uint8Array(graphStateArrayBuffer))
-
-  console.log("AI: ", result.messages[result.messages.length - 1].content);
+  const filePath = "./graphState.png";
+  writeFileSync(filePath, new Uint8Array(graphStateArrayBuffer));
 }
 
 main();
